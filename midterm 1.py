8fb3c5b5dbad56ca1a00d9c49947c89c7676cc46
paulinatorresh <paulimela@icloud.com>
2025-05-02 16:46:21 +0200

    creation

--- midterm 1.py
+++ midterm 1.py
@@ -1,1 +1,12 @@
-test
+#question 1: examples
+
+class Point:
+    def __init__(self, x, y):          # magic (__init__)
+        self.x = x
+        self.y = y
+
+    def distance_orig(self):           # regular method
+        return (self.x**2 + self.y**2) ** 0.5
+
+    def __str__(self):                 # magic (__str__)
+        return f"point({self.x}, {self.y})"
